656faaf6d93d7dd9d5414c0a628e59af14cd035b
jennadaly <jenna.m.daly@gmail.com>
2017-05-11 10:15:54 -0400

    Initial commit.

---
+++ scripts/pre-k-students-psis-processing.R
@@ -0,0 +1,56 @@
+library(dplyr)
+library(datapkg)
+library(RCurl)
+library(httr)
+
+##################################################################
+#
+# Processing Script for Pre-K Students - PSIS
+# Created by Jenna Daly
+# On 05/11/2017
+#
+##################################################################
+
+#This data set is dependent on the Student Enrollment by Grade data set. 
+#Student Enrollment should be updated before this data set can be updated. 
+
+#Setup environment
+sub_folders <- list.files()
+data_location <- grep("raw", sub_folders, value=T)
+path_to_raw_data <- (paste0(getwd(), "/", data_location))
+
+#Fetch student enrollment files from Github
+req <- GET("https://api.github.com/repos/CT-Data-Collaborative/student-enrollment-by-grade/git/trees/master?recursive=1")
+stop_for_status(req)
+filelist <- unlist(lapply(content(req)$tree, "[", "path"), use.names = F)
+student_enrollment_csvs <- grep("data/student", filelist, value = TRUE, fixed = TRUE)
+
+#Bring all student enrollment files into R
+url <- paste0("https://raw.githubusercontent.com/CT-Data-Collaborative/student-enrollment-by-grade/master/", student_enrollment_csvs)
+path_to_student_enroll <- getURL(url)
+student_enroll <- read.csv(text = path_to_student_enroll)
+
+#Save student enrollment data set to raw folder
+write.table(
+  student_enroll,
+  file.path(path_to_raw_data, "student_enrollment_by_grade_2008-2016.csv"),
+  sep = ",",
+  row.names = F
+)
+
+#Read in student enrollment, and isolate pre-k 
+psis <- student_enroll[student_enroll$Grade == "Pre-Kindergarten",]
+
+#Conform to PSIS settings
+psis$Grade <- NULL
+psis$Variable <- "Pre-K Students"
+names(psis)[names(psis) == "Measure.Type"] <- "Measure Type"
+
+#Write CSV
+write.table(
+  psis,
+  file.path(getwd(), "data", "pre-k-students-psis_2008-2017.csv"),
+  sep = ",",
+  row.names = F
+)
+
